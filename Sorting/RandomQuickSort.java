d93d76bd7ff4293bb1a74ba4e173d5a55ab8d497
Umesh Patil <52061782+umeshpatildondaicha@users.noreply.github.com>
2024-03-17 00:03:09 +0530

    Create RandomQuickSort.java

---
+++ Sorting/RandomQuickSort.java
@@ -0,0 +1,52 @@
+package Sorting;
+import java.util.Random;
+
+public class RandomQuickSort {
+
+    public static void quickSort(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        Random rand = new Random();
+        int randomIndex = low + rand.nextInt(high - low + 1);
+        int pivot = arr[randomIndex];
+        swap(arr, randomIndex, high);
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        quickSort(arr);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
